3baf5adfa68bba070ee314420d7ff6babc421e2b
henriquecorte21 <henrique.corte@neoprospecta.com>
2023-10-01 20:41:32 -0300

    feat: change pdfs and custom language

--- js/custom_language.js
+++ js/custom_language.js
@@ -34,55 +34,16 @@ $(function () {
         langButtonListen();
       }
     } else {
-    //   $('[lang="pt"]').hide();
-      // no cookie set, so detect language based on location.
-    //   if ("geolocation" in navigator) {
-    //     // geolocation is available
-    //     navigator.geolocation.getCurrentPosition(function (position) {
-    //       // accepted geolocation so figure out which country
-    //       var lat = position.coords.latitude,
-    //           lng = position.coords.longitude;
-
-    //       $.getJSON('http://maps.googleapis.com/maps/api/geocode/json?latlng='+lat+','+lng+'&sensor=true'+'&'+key, null, function (response) {
-            
-    //         if (response.results.length) {
-    //             console.log(response.results)
-    //             var country = response.results[response.results.length-1].formatted_address;
-    //             if (country ===  'Brazil' || country === 'Portugal') {
-    //               $('[lang="en"]').hide();
-    //               $('[lang="pt"]').show();
-    //               $.cookie('lang', 'pt', { expires: 7 });
-    //               langButtonListen();
-    //             } else {
-    //                 $.cookie('lang', 'en', { expires: 7 });
-    //                 langButtonListen();
-    //             }
-    //         } else {
-    //             $.cookie('lang', 'en', { expires: 7 });
-    //             langButtonListen();
-    //         }
-    //       }).fail(function (err) {
-    //         console.log('error: '+err);
-    //         $.cookie('lang', 'en', { expires: 7 });
-    //         langButtonListen();
-    //       });
-    //     },
-    //     function (error) {
-    //       if (error.code == error.PERMISSION_DENIED) {
-    //         // denied geolocation
-    //         $.cookie('lang', 'en', { expires: 7 });
-    //         langButtonListen();
-    //       } else {
-    //         console.log('Unknown error. Defaulting to English!');
-    //         $.cookie('lang', 'en', { expires: 7 });
-    //         langButtonListen();
-    //       }
-    //     });
-    //   } else {
-        // geolocation IS NOT available
-        $('[lang="pt"]').hide();
-        $.cookie('lang', 'en', { expires: 7 });
-        langButtonListen();
-    //   }
+        var url_ = new URL(location.href);
+        var get_lang = url_.searchParams.get("lang");
+        if (get_lang == "pt") {
+            $('[lang="en"]').hide();
+            $.cookie('lang', 'pt', { expires: 7 });
+            langButtonListen();
+        } else {
+            $('[lang="pt"]').hide();
+            $.cookie('lang', 'en', { expires: 7 });
+            langButtonListen();
+        }
     }
 });
